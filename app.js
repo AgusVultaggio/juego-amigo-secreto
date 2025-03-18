a2e32fd38a95faca4af53ab54e243acf3e80731f
Agustina <agustinavultaggio@gmail.com>
2025-03-18 19:55:27 -0300

    modifica app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    if (!input.value.trim()) return alert("Por favor, inserte un nombre.");
+    
+    amigos.push(input.value.trim());
+    input.value = "";
+    mostrarLista();
+}
+
+function mostrarLista() {
+    document.getElementById("listaAmigos").innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+}
+
+function sortearAmigo() {
+    if (!amigos.length) return alert("Agrega al menos un nombre antes de sortear.");
+    document.getElementById("resultado").innerHTML = `<li> ${amigos[Math.floor(Math.random() * amigos.length)]} es el amigo secreto!</li>`;
+}
